1e4d784b07e341dcf97133eed158e317b967bb54
AnaisSalvador <104086322+AnaisSalvador@users.noreply.github.com>
2023-09-07 15:43:02 -0500

    Add files via upload

---
+++ Lab3_20202152/src/main/java/com/example/lab3_20202152/Lab320202152Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20202152;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320202152Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab320202152Application.class, args);
+	}
+
+}
---
+++ Lab3_20202152/src/test/java/com/example/lab3_20202152/Lab320202152ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20202152;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320202152ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
