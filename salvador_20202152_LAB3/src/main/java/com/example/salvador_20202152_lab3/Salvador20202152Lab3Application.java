444fed9dc628085219f01b020ae4980f03e802d2
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2023-09-07 15:50:14 -0500

    Initial commit

---
+++ salvador_20202152_LAB3/src/main/java/com/example/salvador_20202152_lab3/Salvador20202152Lab3Application.java
@@ -0,0 +1,13 @@
+package com.example.salvador_20202152_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Salvador20202152Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Salvador20202152Lab3Application.class, args);
+	}
+
+}
---
+++ salvador_20202152_LAB3/src/test/java/com/example/salvador_20202152_lab3/Salvador20202152Lab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salvador_20202152_lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Salvador20202152Lab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
